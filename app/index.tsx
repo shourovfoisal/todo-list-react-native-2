032625964d9be956b8877d349d2d7dece3e7d3e6
Shourov Foisal <shourovfoisal@gmail.com>
2025-07-12 21:47:39 +0600

    do the basic stylings

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,14 @@
 import { Ionicons } from "@expo/vector-icons";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import Checkbox from "expo-checkbox";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -37,18 +46,44 @@ export default function Index() {
   ];
 
   return (
-    <SafeAreaView
-      style={styles.container}
-    >
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Ionicons name="menu" size={24} color="#333" />
+        <TouchableOpacity
+          onPress={() => {
+            alert("Clicked!");
+          }}
+        >
+          <Ionicons name="menu" size={24} color="#333" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={{ uri: "https://xsgames.co/randomusers/avatar.php?g=male" }}
+            style={{ width: 40, height: 40, borderRadius: 20 }}
+          />
+        </TouchableOpacity>
       </View>
+
+      <View style={styles.searchBar}>
+        <Ionicons name="search" size={24} color="#333" />
+        <TextInput
+          placeholder="Search Todo"
+          style={styles.searchInput}
+          clearButtonMode="always"
+        />
+      </View>
+
       <FlatList
         data={todoData}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.title}</Text>
+          <View style={styles.todoContainer}>
+            <View style={styles.todoInfoContainer}>
+              <Checkbox value={item.isDone} />
+              <Text style={styles.todoText}>{item.title}</Text>
+            </View>
+            <TouchableOpacity onPress={() => { alert("Deleted " + item.id) }}>
+              <Ionicons name="trash" size={24} color="red" />
+            </TouchableOpacity>
           </View>
         )}
       />
@@ -60,9 +95,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#f5f5f5",
   },
   header: {
-    marginBottom: 20
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  searchBar: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    borderRadius: 10,
+    gap: 10,
+    marginBottom: 20,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+    color: "#333",
+  },
+  todoContainer: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 10,
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  todoInfoContainer: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+  todoText: {
+    fontSize: 16,
+    color: "#333"
   }
-})
+});
